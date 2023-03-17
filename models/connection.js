aba29562bad534424b4f5f8fce8659ea9fa59a32
julietter11 <rouvejuliette@gmail.com>
2023-03-17 22:10:19 +0100

    test2

--- models/connection.js
+++ models/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectionString = process.env.CONNECTION_STRING;
 
-//const connectionString ='mongodb+srv://pommedepin:petitours122.@pommedapi.yct1yr1.mongodb.net/mymeds'
+
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
