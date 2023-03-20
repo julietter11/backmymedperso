02c11f9fc5d22c887f31fa9141ba932eb1e8bba4
julietter11 <rouvejuliette@gmail.com>
2023-03-20 23:22:53 +0100

    cloudinaryessaifinal

--- routes/photos.js
+++ routes/photos.js
@@ -59,24 +59,35 @@ router.get("/upload/:token", (req, res) => {
   });
   
   //supprimer photo utilisateur dans la BDD 
-  router.put("/deletePhoto/:token", (req, res) => {
+  router.put("/deletePhoto/:token", async (req, res) => {
     const { token } = req.params;
     const { url } = req.body;
   
-    User.findOne({ token: token }).then((user) => {
+    try {
+      const user = await User.findOne({ token: token });
       if (user === null) {
         res.json({ result: false, error: "Utilisateur non trouvé" });
         return;
       }
   
-      if (user.ordonnances.includes(url)) {
-        User.updateOne({ _id: user._id }, { $pull: { ordonnances: url } }).then(
-          () => {
-            res.json({ result: true, message: "Ordonnance supprimée" });
-          }
-        );
+      if (!user.ordonnances.includes(url)) {
+        res.json({ result: false, error: "La photo n'existe pas" });
+        return;
       }
-    });
+  
+      // Supprimer la photo de Cloudinary
+      const public_id = url.split('/').pop().split('.')[0]; // Extraire l'ID public de l'image de son URL
+      await cloudinary.uploader.destroy(public_id);
+  
+      // Mettre à jour la liste d'ordonnances de l'utilisateur
+      await User.updateOne({ _id: user._id }, { $pull: { ordonnances: url } });
+  
+      res.json({ result: true, message: "Ordonnance supprimée" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Une erreur s\'est produite lors de la suppression de la photo' });
+    }
   });
+  
 
 module.exports = router;
